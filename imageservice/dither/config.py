33b6c62b9ef42b87edec4405d592c2b847b7229e
jacob <j.bartoszek.jb@gmail.com>
2023-05-12 09:47:11 +0200

    smh

--- imageservice/dither/config.py
+++ imageservice/dither/config.py
@@ -5,8 +5,7 @@ class Config:
     KAFKA_SERVER = os.getenv("KAFKA_SERVER")
     KAFKA_DITHER_TOPIC = os.getenv("KAFKA_DITHER_TOPIC")
     KAFKA_IMAGE_TOPIC = os.getenv("KAFKA_IMAGE_TOPIC")
-    POSTGRES_URL = "postgresql://kriskrawiec99:mCe7uBwErMd9@ep-orange-snow-930075.eu-central-1.aws.neon.tech/neondb?options=project%3Dep-orange-snow-930075"
-    # POSTGRES_URL = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"
+    POSTGRES_URL = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"
     CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
     CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
     CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
